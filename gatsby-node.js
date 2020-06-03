14f32352d4856f4f1aa48d5228fbf2c0d35a2b77
SResok <simonresok@hotmail.com>
2020-06-03 16:46:53 +0200

    Implemented wakatime :D

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,15 @@
-const path = require(`path`)
+const path = require(`path`);
 const _ = require("lodash");
-const { createFilePath } = require(`gatsby-source-filesystem`)
+const axios = require("axios");
+const crypto = require("crypto");
+const { createFilePath } = require(`gatsby-source-filesystem`);
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
 
-  const blogPost = path.resolve(`./src/templates/blog-post.js`)
-  const tagPage = path.resolve(`./src/templates/tag-page.js`)
-  
+  const blogPost = path.resolve(`./src/templates/blog-post.js`);
+  const tagPage = path.resolve(`./src/templates/tag-page.js`);
+
   return graphql(
     `
       {
@@ -31,16 +33,17 @@ exports.createPages = ({ graphql, actions }) => {
     `
   ).then(result => {
     if (result.errors) {
-      throw result.errors
+      throw result.errors;
     }
 
     // Create blog posts pages.
-    const posts = result.data.allMarkdownRemark.edges
+    const posts = result.data.allMarkdownRemark.edges;
     const tagSet = new Set();
 
     posts.forEach((post, index) => {
-      const previous = index === posts.length - 1 ? null : posts[index + 1].node
-      const next = index === 0 ? null : posts[index - 1].node
+      const previous =
+        index === posts.length - 1 ? null : posts[index + 1].node;
+      const next = index === 0 ? null : posts[index - 1].node;
 
       // Get tags for tags pages.
       if (post.node.frontmatter.tags) {
@@ -48,7 +51,7 @@ exports.createPages = ({ graphql, actions }) => {
           tagSet.add(tag);
         });
       }
-      
+
       createPage({
         path: post.node.fields.slug,
         component: blogPost,
@@ -58,7 +61,7 @@ exports.createPages = ({ graphql, actions }) => {
           next
         }
       });
-    })
+    });
 
     // Create tags pages.
     tagSet.forEach(tag => {
@@ -71,20 +74,53 @@ exports.createPages = ({ graphql, actions }) => {
       });
     });
 
-
-    return null
-  })
-}
-
+    return null;
+  });
+};
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
+  const { createNodeField } = actions;
 
   if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
+    const value = createFilePath({ node, getNode });
     createNodeField({
       name: `slug`,
       node,
       value: `/blog${value}`
-    })
+    });
   }
-}
+};
+
+exports.sourceNodes = async ({ boundActionCreators }) => {
+  const { createNode } = boundActionCreators;
+
+  const fetchWakaTimeStats = () =>
+    axios.get(`https://wakatime.com/api/v1/users/@simonresok/stats`);
+  const res = await fetchWakaTimeStats();
+
+  // map into these results and create nodes
+  res.data.data.languages.map((language, i) => {
+    const languageNode = {
+      id: `${i}`,
+      parent: `__SOURCE__`,
+      internal: {
+        type: `WakaTimeLanguage`
+      },
+      children: [],
+
+      name: language.name,
+      percent: language.percent
+    };
+
+    const contentDigest = crypto
+      .createHash(`md5`)
+      .update(JSON.stringify(languageNode))
+      .digest(`hex`);
+    // add it to languageNode
+    languageNode.internal.contentDigest = contentDigest;
+
+    // Create node with the gatsby createNode() API
+    createNode(languageNode);
+  });
+
+  return;
+};
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,26 @@
-import React from "react"
-import { graphql, StaticQuery } from "gatsby"
+import React from "react";
+import { graphql, StaticQuery } from "gatsby";
 // import Img from "gatsby-image"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+const WakaTimeLanguages = props => {
+  const languages = props.languages;
+  return (
+    <ul>
+      {languages.map(language => (
+        <li key={language.node.id}>
+          {language.node.name} ({language.node.percent}%)
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 const AboutPage = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
+  const siteTitle = data.site.siteMetadata.title;
+  const languages = data.allWakaTimeLanguage.edges;
   return (
     <Layout title={siteTitle} location={location}>
       <SEO title="About" keywords={[`blog`, `gatsby`, `javascript`, `react`]} />
@@ -14,16 +28,42 @@ const AboutPage = ({ data, location }) => {
       <header className="page-head">
         <h1 className="page-head-title">Over mij</h1>
         <p className="page-head-description">
-          Ik ben Simon Resok. Ik ben als front-end web developer
-          gespecialiseerd in onder andere HTML, CSS, JavaScript en toegankelijkheid voor iedereen.
+          Ik ben Simon Resok. Ik ben als front-end web developer gespecialiseerd
+          in onder andere HTML, CSS, JavaScript en toegankelijkheid voor
+          iedereen.
         </p>
+
+        <p>
+          Momenteel aan het schrijven{" "}
+          <small>
+            (Via{" "}
+            <a
+              href="https://wakatime.com/@simonresok"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              WakaTime
+            </a>
+            )
+          </small>
+        </p>
+        <WakaTimeLanguages languages={languages} />
       </header>
     </Layout>
   );
-}
+};
 
 const indexQuery = graphql`
   query {
+    allWakaTimeLanguage {
+      edges {
+        node {
+          id
+          name
+          percent
+        }
+      }
+    }
     site {
       siteMetadata {
         title
@@ -39,7 +79,7 @@ const indexQuery = graphql`
       }
     }
   }
-`
+`;
 
 export default props => (
   <StaticQuery
@@ -48,4 +88,4 @@ export default props => (
       <AboutPage location={props.location} data={data} {...props} />
     )}
   />
-)
+);
